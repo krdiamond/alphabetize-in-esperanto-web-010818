508eb3726d5af49e455e31af20d73d199e9eb0c7
krdiamond <github email address>
2017-12-30 13:58:52 -0500

    solved lab

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
+require "pry"
+
 def alphabetize(arr)
-  # code here
+    esp_alph = " abcĉĉdefgĝĝhĥĥijĵĵklmnoprsŝŝtuŭŭvz"
+    
+    arr.sort_by do |string| #looks at each string individually to eventually sort them by the below code
+        string.chars.map do |letter| # looks at each letter in the string individually to figure out how to sort
+            esp_alph.index(letter)
+            #looks at each letter in the custom alphabet and finds the index of each letter in arr's strings
+            #the sort_by method then sorts by this found index number. By maping the custom alphabet you can sort by the numbers instead of the letters. You can custom sort anything by doing it this way.
+        end
+    end
+    #with the sort_by method it will output the sorted answer. No need to return anything.
 end
